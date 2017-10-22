c2d12a84e95f937f2f707afb7a00e4aab9da0ee1
Kobi Shmueli <kobi@dal.net>
2017-10-22 04:32:19 +0300

    Added IsValidNick() function.

--- src/funcs/funcs.go
+++ src/funcs/funcs.go
@@ -14,3 +14,19 @@ func getunixtime() (res int32) {
   res = int32(time.Now().Unix())
   return
 }
+
+func IsValidNick(nick string)(bool) {
+  nicklen := len(nick)
+  if nicklen < 1 || nicklen > types.NICKLEN { return false }
+  if nick[0] == '-' { return false }
+  if nick[0] >= '0' && nick[0] <= '9' { return false }
+
+  for i := 0; i < nicklen; i++ {
+    if nick[i] >= 'A' && nick[i] < '~' { continue }
+    if nick[i] >= '0' && nick[i] <= '9' { continue }
+    if nick[i] == '-' { continue }
+    return false
+  }
+
+  return true
+}
